4cb5bfecd79cd6611b21d62678d03a7f5ff5c311
Marcelo Rossini <marcelo_rossini@outlook.com>
2025-03-20 22:18:23 -0300

    página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ import Menu from '@/components/ui/menu';
 import AfterBefore from "@/components/ui/after-before";
 import { FaWhatsapp } from "react-icons/fa";
 import { CollapsibleItem } from "@/components/ui/collapsible";
+import Asset1 from "@/assets/1.svg"
 import Teste from "@/assets/2.svg"
+import SwipperAnimation from "@/assets/others/swipper-animation.gif"
+import HomeBackground from "@/assets/others/home-background.png"
+import Logo from "@/assets/logo.svg"
+
 import {
   Carousel,
   CarouselContent,
@@ -17,37 +22,49 @@ import { Card, CardContent } from "@/components/ui/card"
 
 export default function Home() {
   return (
-    <div>
-      <Section className="bg-gradient-to-b from-[#d2b9a5] via-[#d2b9a5] via-35% to-[#a9856d]">
-        <div className="w-full h-full">
-          <Menu />
-          <div className="flex flex-col gap-4">
-            <p className="text-4xl text-white font-light">
-              <span className="text-w">Você em harmonia<br /> com a </span><strong>sua beleza</strong>
-            </p>
-            <button className="w-fit text-white rounded-full border-2 py-2 px-4 flex items-center gap-2 text-2xl"><FaWhatsapp size={28} /> Agendar consulta</button>
+    <div className="relative overflow-hidden">
+      <Section className="relative bg-gradient-to-b from-[#d2b9a5] via-[#d2b9a5] via-35% to-[#a9856d]">
+        <Menu />
+        <Image src={Logo} alt="imagem" className="pt-20 px-4 z-10 relative" />
+        <div className="absolute top-0 left-0 w-full h-full"><Image src={Asset1} alt="imagem" className="-mt-40 w-full h-full blur-md opacity-80" /></div>
+        <div className="absolute inset-x-0 bottom-0">
+          <div className="w-full h-full flex items-end relative z-20 p-8 pb-20">
+            <div className="flex flex-col gap-4">
+              <p className="text-4xl text-white font-light drop-shadow-lg">
+                <span>Você em harmonia<br /> com a </span><strong>sua beleza</strong>
+              </p>
+              <button className="flex w-fit items-center gap-2 px-4 py-2 text-2xl text-white border-2 rounded-full drop-shadow-lg">
+                <FaWhatsapp size={28} /> Agendar consulta
+              </button>
+            </div>
           </div>
+          <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent via-[#a9856d] via-35% to-[#a9856d]" />
         </div>
+        <Image src={HomeBackground} alt="imagem" className="absolute bottom-0 left-0 z-0 w-full pb-16 lg:max-w-[80%]" />
       </Section>
       <Section className="bg-[#eee9e3]">
         <Page title={<h1 className="text-3xl">Conheça nossos<br /><span className="text-primary">procedimentos</span></h1>}>
-          <Image src={Teste} alt="teste" className="w-80 absolute -top-30 -right-50 z-0" />
-          <div className="relative py-8 z-10">
-            <Carousel className="w-full max-w-xs mx-auto">
-              <CarouselContent>
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <CarouselItem key={index}>
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-4xl font-semibold">{index + 1}</span>
-                      </CardContent>
-                    </Card>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
+          <Image src={Teste} alt="teste" className="absolute -top-40 -right-50 z-10 w-80" />
+          <div className="relative w-full h-full py-8 border-2 border-[#a9856d]">
+            <div className="absolute -left-4 top-0 w-screen">
+              <Carousel
+                opts={{
+                  align: "start",
+                  loop: true,
+                }}
+                className="w-full"
+              >
+                <CarouselContent className="w-full">
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index}>
+                      <img src={`https://placehold.co/400`} alt="teste" className="w-full h-full object-cover" />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="left-0" />
+                <CarouselNext className="right-2" />
+              </Carousel>
+            </div>
           </div>
         </Page>
       </Section>
@@ -70,40 +87,13 @@ export default function Home() {
             </div>
           </div>
           <div className="grid grid-cols-3 gap-1">
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
+            <div className="w-28 h-28 bg-[#a9856d] rounded-xl">
               a
             </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
+            <div className="w-28 h-28 bg-[#a9856d] rounded-xl">
               a
             </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
-              a
-            </div>
-            <div className="w-28 h-28 bg-red-500 rounded-xl">
+            <div className="w-28 h-28 bg-[#a9856d] rounded-xl">
               a
             </div>
           </div>
--- src/components/ui/menu.tsx
+++ src/components/ui/menu.tsx
@@ -8,7 +8,7 @@ export default function Menu() {
     useEffect(() => {
         const handleScroll = () => {
             const scrollPosition = window.scrollY
-            setIsSticky(scrollPosition > 100) 
+            setIsSticky(scrollPosition > 10) 
         }
 
         window.addEventListener('scroll', handleScroll)
@@ -16,10 +16,8 @@ export default function Menu() {
     }, [])
 
     return (
-        <nav className={`w-full bg-[#d2b9a5] p-4 transition-all duration-300 ${
-            isSticky ? 'fixed top-0 left-0 right-0 z-50' : 'relative'
-        }`}>
-            <ul className="flex justify-between items-center divide-x-2">
+        <nav className={`w-full p-8 transition-all duration-300 fixed top-0 left-0 right-0 z-100 ${isSticky ? 'bg-[#d2b9a5]': ''}`}>
+            <ul className="flex justify-between items-center divide-x-2 divide-black">
                 <li className="flex-1 text-center px-4">HOME</li>
                 <li className="flex-1 text-center px-4">TRATAMENTOS</li>
                 <li className="flex-1 text-center px-4">CONTATO</li>
